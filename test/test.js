ef44613c9e2bc774d864c4663f6b3ab4e63c173d
Marcos Volpi <mark.landia@gmail.com>
2020-03-14 16:16:33 -0300

    Add test

--- test/test.js
+++ test/test.js
@@ -12,17 +12,26 @@ describe('loading express', function () {
   afterEach(function () {
     server.close();
   });
-  it('responds to /currentcurrent', function testSlash(done) {
+  it('responds to /currentcurrent without city', function testSlash(done) {
     request(server)
       .get('/current')
       .expect(200, done);
     });
-    it('responds to /forecast', function testSlash(done) {
+    it('responds to /forecast without city', function testSlash(done) {
         request(server)
-          .get('/forecast')
-          .expect(200, done);
-        });
-          
+        .get('/forecast')
+        .expect(200, done);
+    });
+    it('responds to currentcurrent with city', function testSlash(done) {
+        request(server)
+        .get('/current/caracas')
+        .expect(200, done);
+    });
+    it('responds to /forecast with city', function testSlash(done) {
+        request(server)
+        .get('/forecast/caracas')
+        .expect(200, done);
+    });          
   it('404 doesnt not exist', function testPath(done) {
     request(server)
       .get('/doesnt/not/exist')
