5d56d164a61647f77a879555353090602084b107
Marcos Volpi <mark.landia@gmail.com>
2020-03-14 16:49:52 -0300

    Edit tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,9 @@
-var expect  = require('chai').expect;
-var request = require('request');
+const expect  = require('chai').expect;
+const request = require('supertest');
+const sould = require('should');
 
-
-
-var request = require('supertest');
 describe('loading express', function () {
-  var server;
+  let server;
   beforeEach(function () {
     server = require('../index');
   });
@@ -15,22 +13,42 @@ describe('loading express', function () {
   it('responds to /currentcurrent without city', function testSlash(done) {
     request(server)
       .get('/current')
-      .expect(200, done);
+      .expect(200)
+      .end(function (err, res){
+        let json = JSON.parse(res.text);
+        json.length.should.equal(1);
+        done();
+      });
     });
     it('responds to /forecast without city', function testSlash(done) {
         request(server)
         .get('/forecast')
-        .expect(200, done);
+        .expect(200)
+        .end(function (err, res){
+            let json = JSON.parse(res.text);
+            json.length.should.equal(5);
+            done();
+        });
     });
     it('responds to currentcurrent with city', function testSlash(done) {
         request(server)
         .get('/current/caracas')
-        .expect(200, done);
+        .expect(200)
+        .end(function (err, res){
+            let json = JSON.parse(res.text);
+            json.length.should.equal(1);
+            done();
+        });
     });
     it('responds to /forecast with city', function testSlash(done) {
         request(server)
         .get('/forecast/caracas')
-        .expect(200, done);
+        .expect(200)
+        .end(function (err, res){
+            let json = JSON.parse(res.text);
+            json.length.should.equal(5);
+            done();
+        });
     });          
   it('404 doesnt not exist', function testPath(done) {
     request(server)
